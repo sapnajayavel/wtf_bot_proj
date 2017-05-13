0fb2b043e432bb0e95f7f5cb15f1009892349407
Sapna Jayavel <Sapna.Jayavel@siriuscom.com>
2017-05-13 07:34:29 +0530

    echo msg

--- modules/fbMessenger.js
+++ modules/fbMessenger.js
@@ -105,6 +105,8 @@ module.exports = {
         if (isEcho) {
             // Just logging message echoes to console
             console.log("Received echo for message %s and app %d with metadata %s",
+                message.payload);
+            console.log("Received echo payload %s",
                 messageId, appId, metadata);
             return;
         } else if (quickReply) {
